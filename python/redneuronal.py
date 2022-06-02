b07c7e2361f95133d6bf266599f03551fe6894ab
Javalog <javalog@gmail.com>
2022-06-02 18:24:37 -0400

    Ejercicio Red Neuronal

---
+++ python/redneuronal.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38],dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100],dtype=float)
+
+capa = tf.keras.layers.Dense(units = 1, input_shape=[1])  #Dense: capa que conecta todas sus neuronas con las de la capa siguiente / units nro de neuronas / input_shape define las neuronas de entrada     /  ESTE ESQUEMA AHORRA LA CAPA DE ENTRADA
+modelo = tf .keras.Sequential([capa])  # modelo secuencial es un modelo básico
+
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+
+print("Comenzando entrenamiento...")
+historial = modelo.fit(celsius,fahrenheit, epochs=1000, verbose=False)
+print("modelo entrenado!")
+
+#No sirve
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+plt.show()
+
+print("Predicción:")
+resultado = modelo.predict([100])
+print("El resultado es" + str(resultado) + " fahrenheit")
+
+print("Variables internas del modelo")
+print(capa.get_weights())
